deaa5b885567a517eec1952c99ffb232370e7458
Fialco <113672867+Fialco@users.noreply.github.com>
2024-03-25 18:03:25 +0200

    Ex5-9 complete

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,31 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_lataaminen_kasvattaa_oikein_saldoa(self):
+        self.maksukortti.lataa_rahaa(500)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 15.0)
+
+    def test_saldo_vahenee_jos_rahaa_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(500)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 5.0)
+
+
+    def test_saldo_ei_muutu_jos_rahaa_ei_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(1100)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+
+
+    def test_rahat_riittivat_palauttaa_true(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(900), True)
+
+
+    def test_rahat_ei_riita_palauttaa_false(self):
+
+        self.assertEqual(self.maksukortti.ota_rahaa(1100), False)
